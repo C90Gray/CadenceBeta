d3870590d9bb3c7a0c631b06be36418954fee709
C90Gray <cmusic.gray@gmail.com>
2018-01-22 20:41:23 -0500

    first commit

---
+++ CadenceBeta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CadenceBeta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cadence, the music networking app.");
+        }
+    }
+}
